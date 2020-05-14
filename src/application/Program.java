df6883c0d47b21191de6948af8ee2af9e06ca446
Gabriel Felix <gsfelixgit@gmail.com>
2020-05-14 18:58:47 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import db.DB;
 import db.DbIntegrityException;
 import model.entities.Departmet;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -10,8 +11,10 @@ import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-
+    Date date = new Date(30/04);
         Departmet departmet = new Departmet(1, "Books");
-        System.out.println(departmet);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", date, 3000.00, departmet);
+        System.out.println(seller);
+
     }
 }
